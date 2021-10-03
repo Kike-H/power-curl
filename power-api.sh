d9f8cd596dceede23f993211dde764a01690184d
Manuel E. Hernandez <kicke10h@gmail.com>
2021-10-02 22:46:30 -0500

    add more paramters

--- power-api.sh
+++ power-api.sh
@@ -5,7 +5,7 @@ longitude=$2
 
 
 if [ $longitude ] && [ $latitude ]; then	
-	curl "https://power.larc.nasa.gov/api/temporal/daily/point?parameters=T2M,T2M_RANGE,T2M_MAX,T2M_MIN&community=RE&longitude=$longitude&latitude=$latitude&start=20211002&end=20211002&format="JSON"" > output.json
+	curl "https://power.larc.nasa.gov/api/temporal/daily/point?parameters=T2M,T2M_RANGE,T2M_MAX,T2M_MIN,ALLSKY_SFC_SW_DWN,ALLSKY_KT&community=RE&longitude=$longitude&latitude=$latitude&start=20201001&end=20201001&format="JSON"" > output.json
 fi
 
 
